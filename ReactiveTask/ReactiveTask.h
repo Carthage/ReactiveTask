5a4f807f50543a426613c1a77d744e38604b9059
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-12-01 14:10:53 -0800

    Bootstrap framework project and workspace

---
+++ ReactiveTask/ReactiveTask.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveTask.h
+//  ReactiveTask
+//
+//  Created by Justin Spahr-Summers on 2014-12-01.
+//  Copyright (c) 2014 Carthage. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ReactiveTask.
+FOUNDATION_EXPORT double ReactiveTaskVersionNumber;
+
+//! Project version string for ReactiveTask.
+FOUNDATION_EXPORT const unsigned char ReactiveTaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveTask/PublicHeader.h>
+
+
