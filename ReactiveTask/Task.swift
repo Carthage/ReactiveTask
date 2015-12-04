be750bda2823a767d95f389435f092a9295805b3
Matt Diephouse <matt@diephouse.com>
2015-12-04 11:21:52 -0500

    Use syntactic sugar for optionals

--- ReactiveTask/Task.swift
+++ ReactiveTask/Task.swift
@@ -67,7 +67,7 @@ extension Task: Hashable {
 
 private func ==<Key: Equatable, Value: Equatable>(lhs: [Key: Value]?, rhs: [Key: Value]?) -> Bool {
 	switch (lhs, rhs) {
-	case let (.Some(lhs), .Some(rhs)):
+	case let (lhs?, rhs?):
 		return lhs == rhs
 		
 	case (.None, .None):
