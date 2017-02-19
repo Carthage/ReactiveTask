f1b166b93cd0f8ceba1136c5662adfaf962cab1b
Syo Ikeda <suicaicoca@gmail.com>
2017-02-19 14:07:23 +0900

    [Package.swift] Add `.git` suffix to dependency urls consistently

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "ReactiveTask",
     dependencies: [
-        .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift", majorVersion: 1),
+        .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1),
     ]
 )
