ed7ebf10e7a26e41584319795b7ddb24519be853
Sho Ikeda <suicaicoca@gmail.com>
2019-05-13 23:02:20 +0900

    Add .swiftlint.yml and .hound.yml, address lint warnings

--- Sources/Task.swift
+++ Sources/Task.swift
@@ -36,17 +36,18 @@ public struct Task {
 		self.workingDirectoryPath = workingDirectoryPath
 		self.environment = environment
 	}
-	
+
 	/// A GCD group which to wait completion
 	fileprivate static let group = DispatchGroup()
-	
+
 	/// wait for all task termination
 	public static func waitForAllTaskTermination() {
-		let _ = Task.group.wait(timeout: DispatchTime.distantFuture)
+		_ = Task.group.wait(timeout: DispatchTime.distantFuture)
 	}
 }
 
 extension String {
+	// swiftlint:disable:next force_try
 	private static let whitespaceRegularExpression = try! NSRegularExpression(pattern: "\\s")
 
 	var escapingWhitespaces: String {
@@ -76,15 +77,11 @@ extension Task: Hashable {
 			&& lhs.environment == rhs.environment
 	}
 
-	public var hashValue: Int {
-		var result = launchPath.hashValue ^ (workingDirectoryPath?.hashValue ?? 0)
-		for argument in arguments {
-			result ^= argument.hashValue
-		}
-		for (key, value) in environment ?? [:] {
-			result ^= key.hashValue ^ value.hashValue
-		}
-		return result
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(launchPath)
+		hasher.combine(arguments)
+		hasher.combine(workingDirectoryPath)
+		hasher.combine(environment)
 	}
 }
 
@@ -100,7 +97,7 @@ private final class Pipe {
 
 	/// A GCD queue upon which to deliver I/O callbacks.
 	let queue: DispatchQueue
-	
+
 	/// A GCD group which to wait completion
 	let group: DispatchGroup
 
@@ -169,7 +166,7 @@ private final class Pipe {
 				if let dispatchData = dispatchData {
 					// Cast DispatchData to Data.
 					// See https://gist.github.com/mayoff/6e35e263b9ddd04d9b77e5261212be19.
-					let nsdata = dispatchData as Any as! NSData
+					let nsdata = dispatchData as Any as! NSData // swiftlint:disable:this force_cast
 					let data = Data(referencing: nsdata)
 					observer.send(value: data)
 				}
@@ -223,8 +220,8 @@ private final class Pipe {
 						let buffer = UnsafeRawBufferPointer(start: bytes, count: data.count)
 						return DispatchData(bytes: buffer)
 					}
-					
-					channel.write(offset: 0, data: dispatchData, queue: self.queue) { (done, data, error) in
+
+					channel.write(offset: 0, data: dispatchData, queue: self.queue) { _, _, error in
 						if error == ECANCELED {
 							observer.sendInterrupted()
 						} else if error != 0 {
@@ -247,7 +244,7 @@ private final class Pipe {
 
 public protocol TaskEventType {
 	/// The type of value embedded in a `Success` event.
-	associatedtype T
+	associatedtype T // swiftlint:disable:this type_name
 
 	/// The resulting value, if the event is `Success`.
 	var value: T? { get }
@@ -264,7 +261,7 @@ public protocol TaskEventType {
 public enum TaskEvent<T>: TaskEventType {
 	/// The task is about to be launched.
 	case launch(Task)
-	
+
 	/// Some data arrived from the task on `stdout`.
 	case standardOutput(Data)
 
@@ -305,7 +302,7 @@ public enum TaskEvent<T>: TaskEventType {
 		switch self {
 		case let .launch(task):
 			return .init(value: .launch(task))
-			
+
 		case let .standardOutput(data):
 			return .init(value: .standardOutput(data))
 
@@ -348,7 +345,7 @@ extension TaskEvent: CustomStringConvertible {
 		switch self {
 		case let .launch(task):
 			return "launch: \(task)"
-			
+
 		case let .standardOutput(data):
 			return "stdout: " + dataDescription(data)
 
@@ -368,7 +365,7 @@ extension SignalProducer where Value: TaskEventType {
 			return taskEvent.producerMap(transform)
 		}
 	}
-	
+
 	/// Ignores incremental standard output and standard error data from the given
 	/// task, sending only a single value with the final, aggregated result.
 	public func ignoreTaskData() -> SignalProducer<Value.T, Error> {
@@ -395,8 +392,10 @@ extension Task {
 	///
 	/// - Returns: A producer that will launch the receiver when started, then send
 	///            `TaskEvent`s as execution proceeds.
-	public func launch(standardInput: SignalProducer<Data, NoError>? = nil,
-					   shouldBeTerminatedOnParentExit: Bool = false) -> SignalProducer<TaskEvent<Data>, TaskError> {
+	public func launch( // swiftlint:disable:this function_body_length cyclomatic_complexity
+		standardInput: SignalProducer<Data, NoError>? = nil,
+		shouldBeTerminatedOnParentExit: Bool = false
+	) -> SignalProducer<TaskEvent<Data>, TaskError> {
 		return SignalProducer { observer, lifetime in
 			let queue = DispatchQueue(label: self.description, attributes: [])
 			let group = Task.group
@@ -518,7 +517,7 @@ extension Task {
 							}
 							group.leave()
 						}
-						
+
 						observer.send(value: .launch(self))
 						process.launch()
 						close(stdoutPipe.writeFD)
--- Tests/ReactiveTaskTests/StringExtensionSpec.swift
+++ Tests/ReactiveTaskTests/StringExtensionSpec.swift
@@ -13,7 +13,7 @@ class StringExtensionSpec: QuickSpec {
 			it("should escape the NULL terminator with the 'symbol for null' glyph (U+2400)") {
 				expect("abc\0".escapingWhitespaces).to(equal("abc␀"))
 			}
-			
+
 			it("should not change the original string if it does not contain whitespaces") {
 				expect("ReactiveTask").to(equal("ReactiveTask"))
 			}
